3b7660063ef3d5ce31cde563c4e7e319c403e39e
jaicabher1 <jcaballeroh02@gmail.com>
2023-12-08 18:31:15 +0100

    add(initial): Score visualizer Test Unfavorable score test

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -30,6 +30,32 @@ class TestScoreAverageFunction(TestCase):
         self.assertEqual(resultado, (1*3 + 2*5) / (3 + 5))
 
     
+    def test_score_average_not_equal(self):
+        visualizer = VisualizerView()
 
+        resultado = visualizer.score_average(voting_id=1)
+
+        self.assertNotEqual(resultado, 200)
+
+    def test_score_average_division_zero(self):
+        self.voting.postproc = [
+            {'option': '0', 'number': '11','votes': '0'},
+            {'option': '1', 'number': '1','votes': '0'},
+            {'option': '2', 'number': '2','votes': '0'},
+            {'option': '3', 'number': '3','votes': '0'},
+            {'option': '4', 'number': '4','votes': '0'},
+            {'option': '5', 'number': '5','votes': '0'},
+            {'option': '6', 'number': '6','votes': '0'},
+            {'option': '7', 'number': '7','votes': '0'},
+            {'option': '8', 'number': '8','votes': '0'},
+            {'option': '9', 'number': '9','votes': '0'},
+            {'option': '10', 'number': '10','votes': '0'},
+        ]
+        self.voting.save()
+
+        visualizer = VisualizerView()
+
+        with self.assertRaises(ZeroDivisionError):
+            self.assertRaises(visualizer.score_average(voting_id=1))
 
     
