c8608ca209b933b279023a3101679cf348f03d61
CarlosNuchera <carlosnuchera98@gmail.com>
2023-12-14 21:29:14 +0100

    refactor(initial): Add multiple voting Refectored and simplified multiple options voting. Modified store and voting modules

--- decide/store/models.py
+++ decide/store/models.py
@@ -14,15 +14,3 @@ class Vote(models.Model):
     def __str__(self):
         return '{}: {}'.format(self.voting_id, self.voter_id)
     
-
-class MultipleVote(models.Model):
-    voting_id = models.PositiveIntegerField()
-    voter_id = models.PositiveIntegerField()
-
-    a = [BigBigField()]
-    b = [BigBigField()]
-
-    voted = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return '{}: {}'.format(self.voting_id, self.voter_id)
