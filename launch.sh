75818a97a220861fff199a34a10d194efae05ae3
[[gonsanmar2]] <[[gonsanmar2@alum.us.es]]>
2023-12-18 13:58:47 +0100

    add(update): Render Deploy
    Migrate census

--- launch.sh
+++ launch.sh
@@ -4,4 +4,5 @@ cp local_settings.deploy.py local_settings.py
 ./manage.py collectstatic --noinput
 ./manage.py makemigrations
 ./manage.py migrate
+./manage.py migrate census
 gunicorn -w 5 decide.wsgi:application --timeout=500
