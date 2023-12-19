8e59d40179bfeed22c28e31ea9a1dc77393d7a43
[[gonsanmar2]] <[[gonsanmar2@alum.us.es]]>
2023-12-19 19:19:59 +0100

    add(update): Render deploy
    modifyed launch.sh

--- launch.sh
+++ launch.sh
@@ -1,9 +1,9 @@
+sudo apt-get install gettext
 cd decide/
 cp local_settings.deploy.py local_settings.py
 ./manage.py createsuperuser --noinput
 ./manage.py collectstatic --noinput
 ./manage.py makemigrations
 ./manage.py migrate
-apt-get install gettext
 ./manage.py compilemessages
 gunicorn -w 5 decide.wsgi:application --timeout=500
